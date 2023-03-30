a9bd6e1638dec8a8e69da866e320d7f2d6c323f7
dulkifal <dulkifilibacker@gmail.com>
2023-03-30 17:29:31 +0530

    added products

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,77 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import "./App.css";
 
-
-
 function App() {
-  console.log(import.meta.env.VITE_CLIENT_ID)
+  const produts = [
+    {
+      id: 1,
+      name: "How to be Great at Anything (Book Cover)",
+      price: 100,
+      image: "/ebook.jpg",
+    },
+    {
+      id: 2,
+      name: " HP Pavilion Gaming 10th Gen Intel Core i5 Processor 15.6-inch FHD Gaming Laptop (8GB/512GB SSD/Windows 10/NVIDIA GTX 1650 4GB/Shadow Black/2.3Kg), 15-dk1020TX",
+      price: 550,
+      image: "/laptop.webp",
+    },
+    {
+      id: 3,
+      name: "Apple iPhone 12 Pro Max 512GB - Pacific Blue",
+      price: 250,
+      image: "/phone.jpeg",
+    },
+    {
+      id: 4,
+      name: "Apple Watch Series 6 (GPS, 44mm) - Space Grey Aluminium Case with Black Sport Band",
+      price: 100,
+      image: "/watch.jpeg",
+    },
+    {
+      id: 5,
+      name: "Apple AirPods Pro",
+      price: 50,
+      image: "/airpods.jpeg",
+    }
+
+  ];
+
   return (
     <div className="App-body">
-      <h1>How to be Great at Anything (E-Book)</h1>
-      <img
-        height="300"
-        src="/ebook.jpg"
-        alt="How to be Great at Anything (Book Cover)"
-      />
-      <p>
-        <span className="book-price">$10.00</span>
-      </p>
-      <PayPalScriptProvider
-        options={{ "client-id": import.meta.env.VITE_CLIENT_ID }}
+      <h1> Special offers Hurrey up</h1>
 
-      >
-        <PayPalButtons
-          createOrder={(data, actions) => {
-            return actions.order.create({
-              purchase_units: [
-                {
-                  amount: {
-                    value: "10.00",
-                  },
-                },
-              ],
-            });
-
-          }}
-          onApprove={async (data, actions) => {
-            const details = await actions.order.capture();
-            const name = details.payer.name.given_name;
-            alert("Transaction completed by " + name);
-          }}
-        />
-      </PayPalScriptProvider>
-     
+      <div className="rows">
+      {produts.map((product) => (
+          <div>
+            <img height="300" src={product.image} alt={product.name} />
+            <p>
+              <span className="book-price">${product.price}</span>
+            </p>
+            <PayPalScriptProvider
+              options={{ "client-id": import.meta.env.VITE_CLIENT_ID }}
+            >
+              <PayPalButtons
+                createOrder={(data, actions) => {
+                  return actions.order.create({
+                    purchase_units: [
+                      {
+                        amount: {
+                          value: product.price,
+                        },
+                      },
+                    ],
+                  });
+                }}
+                onApprove={async (data, actions) => {
+                  const details = await actions.order.capture();
+                  const name = details.payer.name.given_name;
+                  alert("Transaction completed by " + name);
+                }}
+              />
+            </PayPalScriptProvider>
+          </div>
+      ))}
+      </div>
     </div>
   );
 }
